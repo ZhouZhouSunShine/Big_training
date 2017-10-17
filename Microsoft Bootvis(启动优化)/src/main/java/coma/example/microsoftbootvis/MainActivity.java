3c3b03898ce5c6fb66ef83fbffa3fafce0e5d030
fanjinwei <724394700@qq.com>
2017-10-17 18:53:52 +0800

    App启动优化

---
+++ Microsoft Bootvis(启动优化)/src/main/java/coma/example/microsoftbootvis/MainActivity.java
@@ -0,0 +1,15 @@
+package coma.example.microsoftbootvis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //显示当前页面布局
+        setContentView(R.layout.activity_main);
+
+    }
+}
